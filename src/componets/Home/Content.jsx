e0cd5b8edda5279f24eab2d04699d267eb82528f
akash <akashak1602@gmail.com>
2023-12-16 01:44:08 +0530

    frimer added commit

--- src/componets/Home/Content.jsx
+++ src/componets/Home/Content.jsx
@@ -3,9 +3,18 @@ import hi from '../../assets/hi.jpg';
 import hi2 from '../../assets/hi2.jpg';
 import hi3 from '../../assets/hi3.jpg';
 import hi4 from '../../assets/hi4.jpg';
+import { useState } from "react";
+import { motion } from "framer-motion";
 
 
+const hiddenMask = `repeating-linear-gradient(to right, rgba(0,0,0,0) 0px, rgba(0,0,0,0) 30px, rgba(0,0,0,1) 30px, rgba(0,0,0,1) 30px)`;
+const visibleMask = `repeating-linear-gradient(to right, rgba(0,0,0,0) 0px, rgba(0,0,0,0) 0px, rgba(0,0,0,1) 0px, rgba(0,0,0,1) 30px)`;
 function Content() {
+
+
+
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [isInView, setIsInView] = useState(false);
     return (
         <div>
             <div class="hidden md:flex  bg-[#ebebed] pt-6 ">
@@ -19,7 +28,7 @@ function Content() {
             <div class="flex  bg-[#ebebed] pt-6">
                 <div class="w-full bg-[#ebebed] h-12 text-center md:text-sm container mx-auto px-4 text-gray-400">
                     <h1 className='md:text-3xl text-2xl'>Our Highlights
-                           </h1>
+                    </h1>
 
                 </div>
 
@@ -40,9 +49,22 @@ function Content() {
                         </span>
                     </div>
                     <div class="md:w-1/3 h-80 p-2">
-                        <img className=" h-full w-full object-cover" src={hi} alt="Connect Icon" />
-
+                        <motion.div
+                            initial={false}
+                            animate={
+                                isLoaded && isInView
+                                    ? { WebkitMaskImage: visibleMask, maskImage: visibleMask }
+                                    : { WebkitMaskImage: hiddenMask, maskImage: hiddenMask }
+                            }
+                            transition={{ duration: 1, delay: 1 }}
+                            viewport={{ once: true }}
+                            onViewportEnter={() => setIsInView(true)}
+                            style={{ maxWidth: '100%', maxHeight: '100%', overflow: 'hidden' }}
+                        >
+                            <img src={hi} alt="" onLoad={() => setIsLoaded(true)} style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
+                        </motion.div>
                     </div>
+
                     <div className="md:w-1/3  h-80 text-center flex flex-col gap-y-5 justify-center p-4">
                         <p className="">Crafting Dreamscapes</p>
                         <h1 className="">Your Personalised Love Story Comes Alive</h1>
@@ -55,9 +77,21 @@ function Content() {
 
             <div className='bg-[#ebebed] pt-2'>
                 <div class="md:flex  container mx-auto">
-                    <div class="md:w-1/3  h-80 p-2">
-                        <img className=" h-full w-full object-cover" src={hi3} alt="Connect Icon" />
-
+                    <div class="md:w-1/3 h-80 p-2">
+                        <motion.div
+                            initial={false}
+                            animate={
+                                isLoaded && isInView
+                                    ? { WebkitMaskImage: visibleMask, maskImage: visibleMask }
+                                    : { WebkitMaskImage: hiddenMask, maskImage: hiddenMask }
+                            }
+                            transition={{ duration: 1, delay: 1 }}
+                            viewport={{ once: true }}
+                            onViewportEnter={() => setIsInView(true)}
+                            style={{ maxWidth: '100%', maxHeight: '100%' }}
+                        >
+                            <img src={hi3} alt="" onLoad={() => setIsLoaded(true)} />
+                        </motion.div>
                     </div>
                     <div className="md:w-1/3  h-80 text-center flex flex-col gap-y-5 justify-center p-4">
                         <p className="">Capturing Moments, Creating Memories Affordably</p>
@@ -66,10 +100,23 @@ function Content() {
                             At Adorn, we seamlessly blend sophistication with affordability by leveraging the latest advancements in photography and videography technology. Our cutting-edge tools aren’t just equipment; they’re the key to weaving your story and making timeless memories accessible to you. With Adorn, quality and affordability converge, making us the go-to choice for premium wedding photography.
                         </span>
                     </div>
-                    <div class="md:w-1/3  h-80 p-2">
-                        <img className=" h-full w-full object-cover" src={hi2} alt="Connect Icon" />
-
-                    </div> </div>
+                    <div class="md:w-1/3 h-80 p-2">
+                        <motion.div
+                            initial={false}
+                            animate={
+                                isLoaded && isInView
+                                    ? { WebkitMaskImage: visibleMask, maskImage: visibleMask }
+                                    : { WebkitMaskImage: hiddenMask, maskImage: hiddenMask }
+                            }
+                            transition={{ duration: 1, delay: 1 }}
+                            viewport={{ once: true }}
+                            onViewportEnter={() => setIsInView(true)}
+                            style={{ maxWidth: '100%', maxHeight: '100%' }}
+                        >
+                            <img src={hi2} alt="" onLoad={() => setIsLoaded(true)} />
+                        </motion.div>
+                    </div>
+                </div>
 
             </div>
             <div className='bg-[#ebebed] pt-2'>
@@ -81,9 +128,21 @@ function Content() {
                             At Adorn, we believe in transforming your digital memories into tangible keepsakes. Our customised photo books, encompassing precious snapshots of your celebration, and high-resolution digital albums, are crafted to endure the passage of time, much like your love. Trust Adron to immortalise your journey in a medium that’s as distinctive as your love story.
                         </span>
                     </div>
-                    <div class="md:w-1/3  h-80 p-2 ">
-                        <img className=" h-full w-full object-cover" src={hi4} alt="Connect Icon" />
-
+                    <div class="md:w-1/3 h-80 p-2">
+                        <motion.div
+                            initial={false}
+                            animate={
+                                isLoaded && isInView
+                                    ? { WebkitMaskImage: visibleMask, maskImage: visibleMask }
+                                    : { WebkitMaskImage: hiddenMask, maskImage: hiddenMask }
+                            }
+                            transition={{ duration: 1, delay: 1 }}
+                            viewport={{ once: true }}
+                            onViewportEnter={() => setIsInView(true)}
+                            style={{ maxWidth: '100%', maxHeight: '100%' }}
+                        >
+                            <img src={hi4} alt="" onLoad={() => setIsLoaded(true)} />
+                        </motion.div>
                     </div>
                     <div className="md:w-1/3  h-80 text-center flex flex-col gap-y-5 justify-center p-4">
                         <p className="">Prompt Delivery</p>
